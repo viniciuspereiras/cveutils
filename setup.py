a52410f791e62f40c80251b22a07bb06d2608b24
Vinicius <69733777+viniciuspereiras@users.noreply.github.com>
2023-05-11 03:01:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
     
 setup(
     name='cveutils',
-    version='0.3',
+    version='0.6',
     description='This library provides functionality for fetching and parsing information about Common Vulnerabilities and Exposures (CVE) using the NIST National Vulnerability Database (NVD) RESTful API.',
     author='Vinicius Pereira (big0us)',
     author_email='vini@cius.xyz',
